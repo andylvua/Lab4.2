ab708065ddadfa3015613e0e794cd10107aa4f49
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2022-10-03 13:05:36 +0300

    Updated `Demo` class

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,2 +1,13 @@
+import lotr.Character;
+import org.reflections.Reflections;
+
+import java.util.Random;
+import java.util.Set;
+
 public class Demo {
+    public static void main(String[] args) {
+        Reflections reflections = new Reflections("lotr");
+        Set<Class<? extends Character >> subTypes = reflections.getSubTypesOf(lotr.Character.class);
+        System.out.println(subTypes.toArray()[new Random().nextInt(subTypes.size())]);
+    }
 }
